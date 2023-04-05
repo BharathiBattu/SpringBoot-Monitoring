617da97d01b088e7f852bd0f3295e7722302ac0b
Battu Bharathi <batbhara@publicisgroupe.net>
2023-04-05 21:22:25 +0530

    SpringBoot Application

---
+++ src/main/java/com/batbhara/sbmonitoring/SbMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.batbhara.sbmonitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbMonitoringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/batbhara/sbmonitoring/SbMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batbhara.sbmonitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
